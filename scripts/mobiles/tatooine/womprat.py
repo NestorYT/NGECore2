0b7f0f71e9eb42d7aa48ab1df80c14357942d364
cody196 <cody.wherry@gmail.com>
2014-03-30 20:39:26 -0400

    Update womprat.py
    Added a custom name

--- scripts/mobiles/tatooine/womprat.py
+++ scripts/mobiles/tatooine/womprat.py
@@ -6,6 +6,7 @@ def addTemplate(core):
 	mobileTemplate = MobileTemplate()
 	templates = Vector()
 	templates.add('object/mobile/shared_womp_rat.iff')
+	mobileTemplate.setCustomName('a womp rat')
 	mobileTemplate.setTemplates(templates)
 	mobileTemplate.setLevel(5)
 	attacks = Vector()
